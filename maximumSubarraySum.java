b591374bddfa2041f4520a2053d2eaae4315a632
Deep <bisht.deepak2838@gmail.com>
2026-06-23 14:15:14 +0530

    Leetcode 53

---
+++ maximumSubarraySum.java
@@ -0,0 +1,38 @@
+class maximumSubarraySum{
+
+//BRUTE FORCE
+
+    // void findSubArray(int arr[]){
+    //     int max=Integer.MIN_VALUE;
+
+    //     for(int i=0;i<arr.length;i++){
+    //         int sum=0;
+    //         for(int j=i;j<arr.length;j++){
+    //             sum+=arr[j];
+    //         }
+    //         max=Math.max(sum,max);
+    //     }
+
+    //     System.out.println(max);
+    // }
+
+// OPTIMIZED    
+    void findSubArray(int arr[]){
+        int fb=0;
+        int mw=0;
+
+        for(int i=0;i<arr.length;i++){
+            fb=Math.max(arr[i], arr[i]+fb);
+            mw=Math.max(fb,mw);
+        }
+        System.out.println(mw);
+
+    }
+
+    public static void main(String args[]){
+        int arr[]={-3,4,-1,2};
+
+        maximumSubarraySum m=new maximumSubarraySum();
+        m.findSubArray(arr);
+    }
+}
