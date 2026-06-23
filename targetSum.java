b7ccef9a211e68f7fc4bbf6eecce46b5d4aa5886
Deepak <deep@Deepaks-MacBook-Air-3.local>
2026-06-23 09:57:07 +0530

    Target Sum

---
+++ targetSum.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+
+class targetSum {
+
+    // void find(int arr[],int target){
+    //     for(int i=0;i<arr.length;i++){
+    //         for(int j=i+1;j<arr.length;j++){
+    //             if(arr[i]+arr[j]==target){
+    //                 System.out.println(arr[i]+" "+arr[j]);
+    //             }
+    //         }
+    //     }
+    // }
+    // void find(int arr[],int target){
+    //     int i=arr[0];
+    //     int j=arr[arr.length-1];
+    //     while(i!=j){
+    //     }
+    // }
+    boolean find(int arr[], int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int complement = target - arr[i];
+            if (hm.containsKey(complement)) {
+                return true;
+            }
+            hm.put(arr[i], i);
+        }
+        return false;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = {2, -3, 4, 8, -1};
+
+        int target = sc.nextInt();
+
+        targetSum ts = new targetSum();
+        System.out.println(ts.find(arr, target));
+    }
+}
