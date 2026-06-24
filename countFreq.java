a1f4af1a3dec5501b0811ed58e7e587caf37294c
Deep <bisht.deepak2838@gmail.com>
2026-06-24 10:28:43 +0530

    tp git the value with specific frequency

--- countFreq.java
+++ countFreq.java
@@ -1,29 +1,32 @@
-class countFreq{
 
-    // void count(int arr[]){
-    //     HashMap<Integer,Integer> hm=new HashMap<>();
+import java.util.*;
 
-    //     for(int x:arr){
-    //         hm.put(x,hm.getOrDefault(x,0)+1);
-    //     }
+class countFreq {
 
-    //     for(int x: hm.keySet()){
-    //         System.out.print(x+":"+hm.get(x)+" ");
-    //     }
-    // }
-    void count(int arr[]){
-        
-        boolean visited=false;
-        for(int i=0;i<arr.length;i++){
-            
+    void count(int arr[]) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int x : arr) {
+            hm.put(x, hm.getOrDefault(x, 0) + 1);
         }
 
+        for (int x : hm.keySet()) {
+            System.out.print(x + ":" + hm.get(x) + " ");
+        }
+
+        System.out.println("");
+        
+        for (int x : hm.keySet()) {
+            if (hm.get(x) == 1) {
+                System.out.print(x + " ");
+            }
+        }
     }
 
-    public static void main(String args[]){
-        int arr[]={2,1,2,5,1,10};
+    public static void main(String args[]) {
+        int arr[] = {2, 1, 2, 5, 1, 10};
 
-        countFreq c=new countFreq();
+        countFreq c = new countFreq();
         c.count(arr);
     }
 }
