6ec33d25bb34aaa950d6d33105e5a8247565af67
Deep <bisht.deepak2838@gmail.com>
2026-06-24 09:59:38 +0530

    count the freq

---
+++ countFreq.java
@@ -0,0 +1,29 @@
+class countFreq{
+
+    // void count(int arr[]){
+    //     HashMap<Integer,Integer> hm=new HashMap<>();
+
+    //     for(int x:arr){
+    //         hm.put(x,hm.getOrDefault(x,0)+1);
+    //     }
+
+    //     for(int x: hm.keySet()){
+    //         System.out.print(x+":"+hm.get(x)+" ");
+    //     }
+    // }
+    void count(int arr[]){
+        
+        boolean visited=false;
+        for(int i=0;i<arr.length;i++){
+            
+        }
+
+    }
+
+    public static void main(String args[]){
+        int arr[]={2,1,2,5,1,10};
+
+        countFreq c=new countFreq();
+        c.count(arr);
+    }
+}
