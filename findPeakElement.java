3968c0c4ca9416384b1e1063f294055922044ebc
Deep <bisht.deepak2838@gmail.com>
2026-07-06 23:46:48 +0530

    Leetcode 162 and 852

---
+++ findPeakElement.java
@@ -0,0 +1,27 @@
+class findPeakElement{
+
+    int find(int arr[]){
+        int low=0;
+        int high=arr.length-1;
+
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>arr[mid+1]){
+                high=mid;
+            }else{
+                low=mid+1;
+            }
+        }
+
+        return low;
+    }
+
+
+    public static void main(String args[]){
+        int arr[]={1,2,1,3,5,6,4};
+
+        findPeakElement f=new findPeakElement();
+        System.out.println(f.find(arr));
+        
+    }
+}
