8d2575b1723b1e0c50db25fd861423a897db2d64
Deep <bisht.deepak2838@gmail.com>
2026-07-08 09:52:07 +0530

    finding the middle of the LL

---
+++ findMiddle.java
@@ -0,0 +1,97 @@
+
+class Node {
+
+    int value;
+    Node next;
+
+    Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+
+    Node head;
+
+    LinkedList() {
+        head = null;
+    }
+
+    void add(int value) {
+        Node newNode = new Node(value);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node itr = head;
+
+            while (itr.next != null) {
+                itr = itr.next;
+            }
+
+            itr.next = newNode;
+        }
+    }
+
+
+    //deletion by value
+    void deleteVal(int x){
+        Node itr=head;
+
+        while(itr!=null && itr.next!=null){
+            if(itr.next.value==x){
+                itr.next=itr.next.next;
+            }
+            itr=itr.next;
+        }
+
+    }
+    //deletion by Node
+    void deleteVal(Node n){
+        n.value=n.next.value;
+        n.next=n.next.next;
+
+    }
+
+    void show() {
+        Node itr = head;
+
+        while (itr != null) {
+            System.out.print(itr.value + " -> ");
+            itr = itr.next;
+        }
+    }
+}
+
+class Solution{
+    Node findM(Node head){
+        Node slow=head;
+        Node fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast.next=slow.next.next;
+        }
+
+        return slow;
+    }
+}
+
+class  findMiddle{
+
+    public static void main(String args[]) {
+        LinkedList l = new LinkedList();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(4);
+        l.add(5);
+        l.show();
+
+        System.out.println();
+        Solution s=new Solution();
+        Node middle=s.findM(l.head);
+        System.out.println(middle.value);
+    }
+}
