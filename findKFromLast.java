d870b246e20d29bf66ee29aa447aeaa3b8d98544
Deep <bisht.deepak2838@gmail.com>
2026-07-08 10:21:21 +0530

    findinf k element from last

---
+++ findKFromLast.java
@@ -0,0 +1,104 @@
+
+class Node {
+
+    int value;
+    Node next;
+
+    Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+
+    Node head;
+
+    LinkedList() {
+        head = null;
+    }
+
+    void add(int value) {
+        Node newNode = new Node(value);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node itr = head;
+
+            while (itr.next != null) {
+                itr = itr.next;
+            }
+
+            itr.next = newNode;
+        }
+    }
+
+
+    //deletion by value
+    void deleteVal(int x){
+        Node itr=head;
+
+        while(itr!=null && itr.next!=null){
+            if(itr.next.value==x){
+                itr.next=itr.next.next;
+            }
+            itr=itr.next;
+        }
+
+    }
+    //deletion by Node
+    void deleteVal(Node n){
+        n.value=n.next.value;
+        n.next=n.next.next;
+
+    }
+
+    void show() {
+        Node itr = head;
+
+        while (itr != null) {
+            System.out.print(itr.value + " -> ");
+            itr = itr.next;
+        }
+    }
+}
+
+
+class Solution{
+    Node find(Node head,int k){
+        Node itr=head;
+        int count=0;
+
+        while(itr!=null){
+            count++;
+            itr=itr.next;
+        }
+        itr=head;
+
+        for(int i=0;i<count-k;i++){
+            itr=itr.next;
+        }
+        return itr;
+
+    }
+}
+
+class findKFromLast{
+
+    public static void main(String args[]) {
+        LinkedList l = new LinkedList();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(4);
+        l.add(5);
+        l.show();
+        System.out.println();
+        
+        int k=2;
+        Solution s=new Solution();
+        Node FindK=s.find(l.head,k);
+        System.out.println(FindK.value);
+    }
+}
