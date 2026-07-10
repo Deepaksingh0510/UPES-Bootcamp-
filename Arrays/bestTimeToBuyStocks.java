fdc70e29c45007d8fe11195dc5cb650447d4cc78
Deep <bisht.deepak2838@gmail.com>
2026-07-10 13:25:10 +0530

    Leetcode 121

---
+++ Arrays/bestTimeToBuyStocks.java
@@ -0,0 +1,28 @@
+class Solution{
+
+    int findDay(int prices[]){
+        int left=0;
+        int right=1;
+        int profit=0;
+
+        while(right<prices.length){
+            if(prices[left]<prices[right]){
+                profit=Math.max(profit,prices[right]-prices[left]);
+            }else{
+                left=right;
+            }   
+
+            right++;
+        }
+        return profit;
+
+    }
+}
+class bestTimeToBuyStocks{
+    public static void main(String[] args) {
+        int prices[]={7,1,5,3,6,4};
+
+        Solution s=new Solution();
+        System.out.print(s.findDay(prices));
+    }
+}
