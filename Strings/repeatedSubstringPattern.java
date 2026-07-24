6e4c8f6228affd86dbb535b75bd6beec5ed8990e
Deep <bisht.deepak2838@gmail.com>
2026-07-24 23:13:32 +0530

    Leetcode 459

--- Strings/repeatedSubstringPattern.java
+++ Strings/repeatedSubstringPattern.java
@@ -8,6 +8,7 @@ public class repeatedSubstringPattern {
     public static void main(String[] args) {
         String s="abcabcabcabcabc";
         repeatedSubstringPattern r=new repeatedSubstringPattern();
+        
         System.out.print(r.find(s));
     }   
 }
